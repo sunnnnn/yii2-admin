63aac364ad46d4600b87665f2f067359c7eed00f
sunnnnn <263021000@qq.com>
2017-05-26 10:44:21 +0800

    修改底部样式

--- basic/views/layouts/footer.php
+++ basic/views/layouts/footer.php
@@ -1,5 +1,5 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
+<footer class="main-footer hidden-xs">
+    <div class="">
     	<b>yii admin </b> Copyright &copy; 2017.<a href="http://www.sunnnnn.com" target="_blank">sunnnnn</a> All rights reserved.
     </div>
 </footer>
