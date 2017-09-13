436709d101e918d732ee69f6e527a67a8b927702
sunnnnn <263021000@qq.com>
2017-09-13 12:41:05 +0800

    update admin.js

--- basic/components/assets/admin/js/admin.js
+++ basic/components/assets/admin/js/admin.js
@@ -10,16 +10,15 @@ $(function(){
 	}
 	
 	if($('.ajax-form').length){
-		document.onkeydown = function(e){ 
-		    var ev = document.all ? window.event : e;
-		    if(ev.keyCode == 13) {
+		$('.ajax-form').find('input[type="text"]').keyup(function(e){
+		    if(e.keyCode == 13) {
 		    	if($('.ajax-form-submit').length){
 		    		$('.ajax-form-submit').click();
 		    	}else{
 		    		return false;
 		    	}
 			}
-		}
+		});
 	}
 	
 	$('.ajax-form-submit').click(function(){
